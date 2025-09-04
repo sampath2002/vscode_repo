e4ce9e61952d739ffce19f11d4615631baa58be0
sampath2002 <koppisettisrisaisampath@gmail.com>
2025-09-04 11:05:25 +0530

    "added delete operation in double linked list"

--- list_operations.py
+++ list_operations.py
@@ -22,8 +22,38 @@ class double_linked_list:
             curr.next = tail
             tail.prev = curr 
         return 
+    def delete(self,val):
+        curr = self.head 
+        if curr.data == val and curr.next is None:
+            self.head = None
+            return
+        elif curr.data == val and  curr.next is not None:
+            self.head = self.head.next
+
+            print(self.head.data)
+            return
+        else:
+            while curr.next:
+                #print(curr.data)
+                if curr.data == val:
+                    #print(curr.data)
+                    curr.prev = curr.next
+
+                temp = curr
+                curr = curr.next
+                
+
+            return 
     1>2>1
     1>2>3>4>1>2>3
+
+    def display(self):
+        curr = self.head
+        print(self.head.data)
+        while curr.next:
+            print(curr.data, end='-->')
+            curr = curr.next
+        print(curr.data, '-->','None')     
 
 class circular_list:
     def __init__(self):
@@ -58,10 +88,45 @@ class circular_list:
             while curr.next != self.head and curr.next is not None:
                 curr = curr.next 
             curr.next = tail
+
             tail.next = self.head 
+            print('<><><>',tail.next.data)
+
+
         return 
     
-            
+    def delete(self,val):
+        curr = self.head 
+        if curr.data == val and curr.next is None:
+            self.head = None
+            return
+        elif curr.data == val and  curr.next is not None:
+            self.head = self.head.next
+
+            #print(self.head.data)
+            return
+        else:
+            while curr.next != self.head:
+                #print(curr.data)
+                if curr.data == val:
+                    print(curr.data)
+                    temp.next = curr.next 
+
+                temp = curr
+                curr = curr.next
+            if curr.data == val:
+                print(curr.data)
+                temp.next = curr.next 
+
+            return 
+        
+    def display(self):
+        curr = self.head
+        print(self.head.data)
+        while curr.next != self.head:
+            print(curr.data, end='-->')
+            curr = curr.next
+        print(curr.data, '-->',curr.next.data)            
 
 
 class LinkedList:
@@ -83,14 +148,25 @@ class LinkedList:
             curr.next = tail
             print(curr.data,'-->',curr.next.data)
 
-l = double_linked_list()
+l1 = double_linked_list()
+l1.append(22)
+l1.append(33)
+l1.append(44)
+l1.append(55)
+l1.delete(22)
+#l1.display()
+l = circular_list()
 l.append(22)
 l.append(33)
 l.append(44)
-l.append(55)
-l1 = circular_list()
+l.display()
+l.delete(22)
+l.display()
+l.append(45)
+l.display()
+
 l3 = [22,33,44,55,66]
-l1.list_to_node(l3)
-print('list BEFORE rotation: ', l3, '\nlist AFTER rotation : ',l1.rotate(100,[22,33,44,55,66]))
+#l1.list_to_node(l3)
+#print('list BEFORE rotation: ', l3, '\nlist AFTER rotation : ',l1.rotate(100,[22,33,44,55,66]))
 
 
